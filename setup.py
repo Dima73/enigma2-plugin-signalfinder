c5e872949182426c756d90e40105a771516f1cca
Taapat <taapat@gmail.com>
2015-10-22 21:07:15 +0300

    Add setup.py and setup_translate.py to build plugin with stuptools

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import setup_translate
+
+
+setup(name = 'enigma2-plugin-systemplugins-signalfinder',
+		version='1.0',
+		author='Dima73',
+		author_email='dima-73@inbox.lv',
+		package_dir = {'SystemPlugins.Signalfinder': 'src'},
+		packages=['SystemPlugins.Signalfinder'],
+		package_data={'SystemPlugins.Signalfinder': ['image/*.png']},
+		description = 'Signal finder for DVB-S2 tuners',
+		cmdclass = setup_translate.cmdclass,
+	)
+
